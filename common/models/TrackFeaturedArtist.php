9a3e5f0e39d64e2bcf4d276b99ffd530fe8f7235
benaventexxx <nexxxhckg@gmail.com>
2026-01-04 17:03:00 +0000

    feat: add TrackFeaturedArtist model

---
+++ common/models/TrackFeaturedArtist.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+class TrackFeaturedArtist extends \yii\db\ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'track_featured_artist';
+    }
+
+    public function rules()
+    {
+        return [
+            [['track_id', 'artist_id'], 'required'],
+            [['track_id', 'artist_id'], 'integer'],
+            [['track_id', 'artist_id'], 'unique', 'targetAttribute' => ['track_id', 'artist_id']],
+
+            // fk -> track
+            [
+                ['track_id'], 'exist', 'skipOnError' => true,
+                'targetClass' => Track::class, 'targetAttribute' => ['track_id' => 'id']
+            ],
+
+            // fk -> artist
+            [
+                ['artist_id'], 'exist', 'skipOnError' => true,
+                'targetClass' => Artist::class, 'targetAttribute' => ['artist_id' => 'id']
+            ],
+        ];
+    }
+
+    // pivot -> track
+    public function getTrack()
+    {
+        return $this->hasOne(Track::class, ['id' => 'track_id']);
+    }
+
+    // pivot -> artist
+    public function getArtist()
+    {
+        return $this->hasOne(Artist::class, ['id' => 'artist_id']);
+    }
+}
+
+
